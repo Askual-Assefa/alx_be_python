1d246f678faababf0acbb13e6ad8896172bff2e7
Askual Assefa <atm.askual@gmail.com>
2024-07-03 10:56:08 -0700

    Personal Finance Calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,3 +1,7 @@
-current_age = int(input("How old are you? "))
-age_in_2050 = current_age + 27
-print(f"In 2050, you will be {age_in_2050} years old.")
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+annual_savings = monthly_savings * 12
+projected_savings = annual_savings + (annual_savings * 0.05)
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
